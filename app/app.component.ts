04a4315a6b4132c3464ed842aecfca89ce27a8b6
fclubnick <fclubnick@gmail.com>
2016-12-29 16:23:02 +0100

    1.0.0

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'hello-world',
+    template: '<h1>Hello World From www.angulartypescript.com</h1><h2>Hallox</h2>'
+})
+export class AppComponent { }
